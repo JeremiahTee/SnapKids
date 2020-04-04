0a2c5e47f793700db3d03d936e703db07b35c4d2
Jeremiah <jeremiahteee@gmail.com>
2020-04-03 21:19:35 -0400

    fox functionality 2/3
    -added sampledata folder taken from repo from the Google codelab tutorial
    
    https://codelabs.developers.google.com/codelabs/augmented-faces-with-sceneform/#2
    
    -upgraded sceneform to 1.15.0 (latest version)
    -added fragment, activity classes
    -deleted fox_face.sfb as it is not working. will generate new .sfb using sampledata folder

--- app/src/main/java/com/haxstar/snapkids/ArFaceFragment.java
+++ app/src/main/java/com/haxstar/snapkids/ArFaceFragment.java
@@ -1,4 +1,41 @@
 package com.haxstar.snapkids;
 
-public class ArFaceFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import com.google.ar.core.Config;
+import com.google.ar.core.Config.AugmentedFaceMode;
+import com.google.ar.core.Session;
+import com.google.ar.sceneform.ux.ArFragment;
+import java.util.EnumSet;
+import java.util.Set;
+
+public class ArFaceFragment extends ArFragment {
+    @Override
+    protected Config getSessionConfiguration(Session session) {
+        Config config = new Config(session);
+        config.setAugmentedFaceMode(AugmentedFaceMode.MESH3D);
+        return config;
+    }
+
+    @Override
+    protected Set<Session.Feature> getSessionFeatures() {
+        return EnumSet.of(Session.Feature.FRONT_CAMERA);
+    }
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        FrameLayout frameLayout =
+                (FrameLayout) super.onCreateView(inflater, container, savedInstanceState);
+
+
+        //Turn off planeDiscoveryController as plane trackables are not supported with the front camera.
+        getPlaneDiscoveryController().hide();
+        getPlaneDiscoveryController().setInstructionView(null);
+
+        return frameLayout;
+    }
 }
